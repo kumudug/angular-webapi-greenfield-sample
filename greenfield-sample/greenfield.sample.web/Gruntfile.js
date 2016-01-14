3d7dd693f03be81e5517eda5abc306847a38b4b4
Kumudu Gunasekara <kumudugunasekara@hotmail.com>
2016-01-14 19:23:25 +1100

    Initial project setup

---
+++ greenfield-sample/greenfield.sample.web/Gruntfile.js
@@ -0,0 +1,22 @@
+﻿module.exports = function (grunt) {
+
+    grunt.initConfig({
+        wiredep: {
+            task: {
+                src: ['index.html'],
+                overrides: {
+                    bootstrap: {
+                        main: [
+                            "dist/css/bootstrap.min.css",
+                            "dist/js/bootstrap.min.js"
+                        ]
+                    },
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-wiredep');
+
+    grunt.registerTask('default', ['wiredep']);
+}
