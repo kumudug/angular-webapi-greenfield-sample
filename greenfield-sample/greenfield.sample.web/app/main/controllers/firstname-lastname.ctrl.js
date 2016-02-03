e149c2dde16c671a68eb346695dc2ae1c33c23d3
Kumudu Gunasekara <kumudugunasekara@hotmail.com>
2016-02-03 18:04:27 +1100

    Initial angular setup

---
+++ greenfield-sample/greenfield.sample.web/app/main/controllers/firstname-lastname.ctrl.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('firstnameLastnameCtrl', firstnameLastnameCtrl);
+
+    firstnameLastnameCtrl.$inject = ['$location']; 
+
+    function firstnameLastnameCtrl($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'firstnameLastnameCtrl';
+
+        activate();
+
+        function activate() { }
+    }
+})();
---
+++ greenfield-sample/greenfield.sample.web/app/main/controllers/main.ctrl.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('mainApp')
+        .controller('mainCtrl', mainCtrl);
+
+    mainCtrl.$inject = [];
+
+    function mainCtrl() {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'mainCtrl';
+        vm.testFunc = testFunc;
+        
+        function testFunc() {
+
+        }
+    }
+})();
